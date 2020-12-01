77797a0808a44bcc45f933b88da2ffc1f5f94e3d
Marina_Kiseleva <marina.k.15@yandex.ru>
2020-12-01 14:58:09 +0400

    Написан Тест1: открывает гугл, ищет "калькулятор"

---
+++ src/test/java/tests/googleCalc1.java
@@ -0,0 +1,23 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.SearchPage;
+
+public class googleCalc1 {
+
+
+    @Test
+    public void test1() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+        driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("Калькулятор", Keys.ENTER);
+        driver.quit();
+
+
+    }
+}
