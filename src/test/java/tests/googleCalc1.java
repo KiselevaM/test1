188bfdfac6cb0c0400b7f271473164930a9a4900
Marina_Kiseleva <marina.k.15@yandex.ru>
2020-12-01 15:59:46 +0400

    Печатает выражение (1+2)*3-40/5, получает результат 1

--- src/test/java/tests/googleCalc1.java
+++ src/test/java/tests/googleCalc1.java
@@ -16,7 +16,19 @@ public class googleCalc1 {
         WebDriver driver = new ChromeDriver();
         driver.get("http://google.com");
         driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("Калькулятор", Keys.ENTER);
-        driver.findElement(By.cssSelector("div[jsname='j93WEe']")).click();
+        driver.findElement(By.cssSelector("div[jsname='j93WEe']")).click();// (
+        driver.findElement(By.cssSelector("div[jsname='N10B9']")).click();// 1
+        driver.findElement(By.cssSelector("div[jsname='XSr6wc']")).click();// +
+        driver.findElement(By.cssSelector("div[jsname='lVjWed']")).click();// 2
+        driver.findElement(By.cssSelector("div[jsname='qCp9A']")).click();// )
+        driver.findElement(By.cssSelector("div[jsname='YovRWb']")).click();// *
+        driver.findElement(By.cssSelector("div[jsname='KN1kY']")).click();// 3
+        driver.findElement(By.cssSelector("div[jsname='pPHzQc']")).click();// -
+        driver.findElement(By.cssSelector("div[jsname='xAP7E']")).click();// 4
+        driver.findElement(By.cssSelector("div[jsname='bkEvMb']")).click();// 0
+        driver.findElement(By.cssSelector("div[jsname='WxTTNd']")).click();// /
+        driver.findElement(By.cssSelector("div[jsname='Ax5wH']")).click();// 5
+        driver.findElement(By.cssSelector("div[jsname='Pt8tGc']")).click();// =
         driver.quit();
 
 
