d58da301c250195639f721a595376697fd51505e
Marina_Kiseleva <marina.k.15@yandex.ru>
2020-12-01 16:29:55 +0400

    Вынесены предусловие и постусловие

--- src/test/java/tests/googleCalc1.java
+++ src/test/java/tests/googleCalc1.java
@@ -1,5 +1,7 @@
 package tests;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -9,11 +11,17 @@ import pages.SearchPage;
 
 public class googleCalc1 {
 
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void init() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
 
     @Test
     public void test1() {
 
-        WebDriver driver = new ChromeDriver();
         driver.get("http://google.com");
         driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("Калькулятор", Keys.ENTER);
         driver.findElement(By.cssSelector("div[jsname='j93WEe']")).click();// (
@@ -29,8 +37,15 @@ public class googleCalc1 {
         driver.findElement(By.cssSelector("div[jsname='WxTTNd']")).click();// /
         driver.findElement(By.cssSelector("div[jsname='Ax5wH']")).click();// 5
         driver.findElement(By.cssSelector("div[jsname='Pt8tGc']")).click();// =
-        driver.quit();
 
-
+        //driver.findElement(By.cssSelector("div[jsname='ubtiRe']"));// поиск строки с выражением
     }
+
+
+        @AfterAll
+        public static void teardown() {
+            driver.quit();
+        }
+
+
 }
